7d85862faac55f4a888c9c6026934cbb49e9c87a
Savio Bajpai <joelbajpai7@gmail.com>
2026-04-25 17:46:19 +0530

    add blog utility to read MDX files and extract metadata

---
+++ lib/blog.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const blogDir = path.join(process.cwd(), "content/blog");
+
+export function getAllPosts(){
+    const files = fs.readdirSync(blogDir);
+
+    return files.map((file) => {
+        const filePath = path.join(blogDir, file);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+
+        const { data } = matter(fileContent);
+
+        return {
+            slug: file.replace(".mdx", ""),
+            ...data
+        };
+    });
+}
